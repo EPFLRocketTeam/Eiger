44d3595f450d5570d92b3cbc3eb47e6e5382447d
Alexandre Devienne <deviennealexandre@gmail.com>
2019-04-15 23:34:59 +0200

    Simplified SD card logging code

--- library/host-board/Src/sd_Logging/sd_sync.c
+++ library/host-board/Src/sd_Logging/sd_sync.c
@@ -15,41 +15,18 @@
 
 #include <string.h>
 
-extern SD_HandleTypeDef hsd;
-
 #define MAX_FOLDER_NUMBER 1000
-
-extern osMessageQId sdLoggingQueueHandle;
-
 volatile bool new_sd_data_ready = 0;
 
 FIL sensorsFile, eventsFile;
 const TCHAR sensor_file_header[] = "CANSeq\tTimestamp[ms]\tID\tData\r\n";
 const TCHAR events_file_header[] = "NOT USED timestamp, event_description\r\n";
+
 volatile char sd_buffer[2048];
-uint8_t file_closed = 0;
-int sdErrorCounter = 0;
 
 osStatus initSdFile ()
 {
   MX_FATFS_Init ();
-
-  FRESULT res; /* FatFs function common result code */
-  uint32_t byteswritten, bytesread; /* File write/read counts */
-
-  //osDelay(portMAX_DELAY);
-
-  for (int i = 3; i >= 0; i--)
-    {
-      if (disk_initialize (0) == 0)
-        {
-          break; //Initialization is valid
-        }
-      else
-        {
-          osDelay (300);
-        }
-    }
 
   if (disk_initialize (0) != 0)
     {
@@ -97,13 +74,14 @@ osStatus initSdFile ()
 
 void TK_sd_sync (void const* pvArgs)
 {
-  init: osDelay (200);
+  osDelay (200);
 
   if (initSdFile () != osOK)
     {
-	  led_set_rgb(255,0,50);
-      osDelay (10 * 1000);
-      goto init;
+	  while (1) {
+    	  led_set_r(50);
+		  osDelay (1000);
+	  }
     }
 
   UINT bytes_written = 0;
@@ -111,8 +89,6 @@ void TK_sd_sync (void const* pvArgs)
   f_write (&eventsFile, events_file_header, strlen (events_file_header), &bytes_written);
 
   uint32_t lastSync = HAL_GetTick ();
-  uint32_t last_execution = 0;
-  uint32_t elapsed;
 
   f_sync (&sensorsFile);
   f_sync (&eventsFile);
@@ -121,91 +97,25 @@ void TK_sd_sync (void const* pvArgs)
   osDelay (500);
   FRESULT result = 0;
 
-  int i = -14;
-
   for (;;)
     {
-      result = 0;
-      /*
-       if (currentState == STATE_TOUCHDOWN && !file_closed)
-       {
-       f_close (&sensorsFile);
-       f_close (&eventsFile);
-       file_closed = 1;
-       for (;;)
-       {
-       osDelay (portMAX_DELAY);
-       }
-       }
-
-       osEvent event = osMessageGet (sdLoggingQueueHandle, 5);
-       if (event.status != osEventMessage)
-       {
-       goto endOfLoop;
-       }
-
-       String_Message* m = event.value.p;
-       vPortFree (m->ptr);
-       */
-
-      // ###############
-      // Here you need to fetch the data and to print it.
-      uint32_t measurement_time = HAL_GetTick ();
       if (new_sd_data_ready) {
-      //sprintf (buffer, "%d\t\tTest print %d\t free heap:\t%d\n", measurement_time, i++, xPortGetFreeHeapSize ());
+    	  int line_size = strlen (sd_buffer);
+    	  result |= f_write (&sensorsFile, sd_buffer, line_size, &bytes_written);
+    	  new_sd_data_ready = 0;
 
-      // ###############
-
-      int line_size = strlen (sd_buffer);
-      if (line_size > 768 || line_size < 0)
-        {
-          goto endOfLoop;
-        }
-      result |= f_write (&sensorsFile, sd_buffer, line_size, &bytes_written);
-      new_sd_data_ready = 0;
-      if ((HAL_GetTick () - lastSync) > 1000) // synchronization every second with the SD card.
-        {
-          result |= f_sync (&sensorsFile);
-          //result |= f_sync (&eventsFile);
-          lastSync = HAL_GetTick ();
-        }
+    	  if ((HAL_GetTick () - lastSync) > 1000) // synchronization every second with the SD card.
+    	  {
+    		  result |= f_sync (&sensorsFile);
+    		  lastSync = HAL_GetTick ();
+    	  }
       }
 
-      endOfLoop: elapsed = HAL_GetTick () - last_execution;
-      uint32_t delay = 5; // Delay of 50ms
-      if (elapsed < delay)
-        {
-          //osDelay (delay - elapsed);
-        }
-      last_execution = HAL_GetTick ();
+      if (result == FR_OK) {
+    	  led_set_g(50);
+      } else {
+    	  led_set_r(255);
+      }
       osDelay(1);
-      /*
-       if (result != FR_OK)
-       { // create a new file in case of problem.  ## does not work correctly yet
-       f_close (&sensorsFile);
-       f_close (&eventsFile);
-
-       HAL_SD_DeInit (&hsd);
-
-       osDelay (100);
-
-       HAL_StatusTypeDef sd_state = HAL_SD_Init (&hsd);
-       if (sd_state == MSD_OK)
-       {
-       if (HAL_SD_ConfigWideBusOperation (&hsd, SDIO_BUS_WIDE_4B) != HAL_OK)
-       {
-       sd_state = MSD_ERROR;
-       }
-       }
-
-       break;
-       }
-       */
-
     }
-
-  //we need to get outside the for loop to correctly deinitialize scope-local data.
-  sdErrorCounter++;
-  goto init;
-
 }
