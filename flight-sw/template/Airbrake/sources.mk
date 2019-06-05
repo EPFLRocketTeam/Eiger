9e4161978cf8999648393de252a5a2a3cfb92aa1
Alberic de Lajarte <albericlajarte@gmail.com>
2019-06-05 10:06:32 +0200

    manual merge with master

---
+++ flight-sw/template/Airbrake/sources.mk
@@ -0,0 +1,38 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+C_UPPER_SRCS := 
+CXX_SRCS := 
+C++_SRCS := 
+OBJ_SRCS := 
+S_SRCS := 
+CC_SRCS := 
+ASM_SRCS := 
+C_SRCS := 
+CPP_SRCS := 
+S_UPPER_SRCS := 
+O_SRCS := 
+CC_DEPS := 
+C++_DEPS := 
+EXECUTABLES := 
+OBJS := 
+C_UPPER_DEPS := 
+CXX_DEPS := 
+S_UPPER_DEPS := 
+C_DEPS := 
+CPP_DEPS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+Drivers/STM32F4xx_HAL_Driver \
+Middlewares/FatFs \
+Middlewares/FreeRTOS \
+Src \
+host-board/Src \
+host-board/Src/Misc \
+host-board/Src/Telemetry \
+host-board/Src/airbrake \
+host-board/Src/sd_Logging \
+startup \
+
