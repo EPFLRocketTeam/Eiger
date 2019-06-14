21c2f509cf187cd458a3c8378bd172c655427412
Alexandre Devienne <deviennealexandre@gmail.com>
2019-06-14 23:05:57 +0200

    Cleanup telemetry send packets functions

--- library/host-board/Src/Misc/telemetry_handling.cpp
+++ library/host-board/Src/Misc/telemetry_handling.cpp
@@ -36,10 +36,9 @@ uint32_t telemetrySeqNumber = 0;
 
 IMU_data  imu  = {{0,0,0},{0,0,0}, 0};
 BARO_data baro = {0,0,0};
-uint32_t last_sensor_update = 0;
+GPS_data gps = {0.0, 0.0, 0.0, 0, 0};
 Telemetry_Message m1;
 Telemetry_Message m2;
-Telemetry_Message m3;
 
 Telemetry_Message createTelemetryDatagram (IMU_data* imu_data, BARO_data* baro_data, uint32_t measurement_time, uint32_t telemetrySeqNumber)
 {
@@ -81,14 +80,46 @@ Telemetry_Message createGPSDatagram (uint32_t seqNumber, GPS_data gpsData)
   return builder.finalizeDatagram ();
 }
 
+bool send_telemetry() {
+	static uint32_t last_update = 0;
+	uint32_t now = HAL_GetTick();
+	bool handled = false;
 
+	if (now - last_update > TELE_TIMEMIN) {
 
+		m2 = createTelemetryDatagram (&imu, &baro, now, telemetrySeqNumber++);
+		if (osMessagePut (xBeeQueueHandle, (uint32_t) &m2, 10) != osOK) {
+			vPortFree(m2.ptr); // free the datagram if we couldn't queue it
+		}
+		last_update = now;
+		handled = true;
+	}
+
+	return handled;
+}
+
+bool send_gps() {
+	static uint32_t last_update = 0;
+	uint32_t now = HAL_GetTick();
+	bool handled = false;
+
+	if (now - last_update > GPS_TIMEMIN) {
+		m1 = createGPSDatagram (telemetrySeqNumber++, gps);
+		if (osMessagePut (xBeeQueueHandle, (uint32_t) &m1, 10) != osOK) {
+			vPortFree(m1.ptr); // free the datagram if we couldn't queue it
+		}
+		last_update = now;
+		handled = true;
+	}
+}
 
 
 bool telemetry_handleGPSData(GPS_data data) {
 	static uint32_t last_update = 0;
 	uint32_t now = HAL_GetTick();
 	bool handled = false;
+
+	gps = data;
 
 	if (now - last_update > GPS_TIMEMIN) {
 		m1 = createGPSDatagram (telemetrySeqNumber++, data);
@@ -98,40 +129,16 @@ bool telemetry_handleGPSData(GPS_data data) {
 		last_update = now;
 		handled = true;
 	}
-	return handled;
+	return send_gps();
 }
 
 
 bool telemetry_handleIMUData(IMU_data data) {
-	uint32_t now = HAL_GetTick();
-	bool handled = false;
-
 	imu = data;
-
-	if (now - last_sensor_update > TELE_TIMEMIN) {
-		m2 = createTelemetryDatagram (&imu, &baro, now, telemetrySeqNumber++);
-		if (osMessagePut (xBeeQueueHandle, (uint32_t) &m2, 10) != osOK) {
-			vPortFree(m2.ptr); // free the datagram if we couldn't queue it
-		}
-		last_sensor_update = now;
-		handled = true;
-	}
-	return handled;
+	return send_telemetry();
 }
 
 bool telemetry_handleBaroData(BARO_data data) {
-	uint32_t now = HAL_GetTick();
-	bool handled = false;
-
 	baro = data;
-
-	if (now - last_sensor_update > TELE_TIMEMIN) {
-		m3 = createTelemetryDatagram (&imu, &baro, now, telemetrySeqNumber++);
-		if (osMessagePut (xBeeQueueHandle, (uint32_t) &m3, 10) != osOK) {
-			vPortFree(m3.ptr); // free the datagram if we couldn't queue it
-		}
-		last_sensor_update = now;
-		handled = true;
-	}
-	return handled;
+	return send_telemetry();
 }
